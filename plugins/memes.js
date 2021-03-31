9dfb2b609faf3fe7d316aa069fa528e6720b24f5
prono69 <faltu9851@gmail.com>
2021-03-31 09:53:08 +0530

    k

--- plugins/memes.js
+++ plugins/memes.js
@@ -74,8 +74,8 @@ else if (Config.WORKTYPE == 'public') {
         var topText, bottomText;
         if (match[1].includes(';')) {
             var split = match[1].split(';');
-            topText = split[1];
-            bottomText = split[0];
+            topText = split[0];
+            bottomText = split[1];
         }
 	    else {
             topText = match[1];
